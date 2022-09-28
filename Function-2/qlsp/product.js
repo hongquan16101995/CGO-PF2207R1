fe48fbbc71fb7cb04693983dd6ed92d416ff03cd
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-09-28 21:23:32 +0700

    check va chạm

--- Function-2/qlsp/product.js
+++ Function-2/qlsp/product.js
@@ -2,12 +2,14 @@ class Product {
     name
     price
     color
+    quantity
     brand
 
-    constructor(name, price, color, brand) {
+    constructor(name, price, color, quantity, brand) {
         this.name = name
         this.price = price
         this.color = color
+        this.quantity = quantity
         this.brand = brand
     }
 }
